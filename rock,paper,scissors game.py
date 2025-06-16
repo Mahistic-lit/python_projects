616d17d82413b78d327208e9103ebd314ff352ec
Mahistic <mehrmahfatima08@gmail.com>
2025-06-16 13:42:57 +0500

    Update rock-paper-scissors game.py

--- rock,paper,scissors game.py
+++ rock,paper,scissors game.py
@@ -4,15 +4,21 @@ Lets play the 'Rock','Paper','Scissors' game with computer!
 The computer has already decided its move!
 NOW ITS YOUR TURN!!!
 ''')
+#genrating computer's choice
 choices=['rock','paper','scissors']
 comp_choice=random.choice(choices)
+
 for index,words in enumerate(choices,start=1):
     print(index,words)
+#asking for user's input
 player_choice=str(input('choose between given list:'))
+#converting it to the lower case
 player_choices=player_choice.lower()
+#incase the user inputs an invalid choice
 if player_choices!='rock' and player_choices!='paper' and player_choices!='scissors':
     print("INVALID CHOICE,PLEASE SELECT BETWEEN 'ROCK','PAPER','SCISSORS'")
     exit()
+#conditions
 if player_choices=='rock' and comp_choice=='scissors':
     print(f'CONGRATULATIONS!,YOU WIN!!,computer choose {comp_choice}')
 elif player_choices=='rock' and comp_choice=='paper':
